26a8676225aea202bab5a47a417c917f9909c5c5
neverendingmilker <neverendingmilker@gmail.com>
2026-08-02 17:51:19 +0200

    Delete src directory

--- src/commands/quiz/handlers/reset.js
+++
@@ -1,8 +0,0 @@
-const quizRepository = require('../../../features/quiz/quizRepository');
-
-async function handleReset(interaction) {
-  await quizRepository.reset(interaction.guildId);
-  await interaction.reply('🔄 Leaderboard reset.');
-}
-
-module.exports = { handleReset };
